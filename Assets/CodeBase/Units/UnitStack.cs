7b42ec00056a07fc64f5f1666dbdae7a4010a075
l34o <vadim.motolko@gmail.com>
2024-07-21 18:23:40 +0300

    Started working on displaying the number of units

--- Assets/CodeBase/Units/UnitStack.cs
+++ Assets/CodeBase/Units/UnitStack.cs
@@ -1,12 +1,16 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitStack : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _amountBar;
     private int Amount = 0;
 
     public void ApplyDamage(int damage)
     {
         Amount -= damage;
+        UpdateBar();
         if (Amount < 0)
         {
             Amount = 0;
@@ -18,5 +22,11 @@ public class UnitStack : MonoBehaviour
     public void AddAmount (int amount)
     {
         Amount += amount;
+        UpdateBar();
+    }
+
+    private void UpdateBar()
+    {
+        _amountBar.text = Amount.ToString();
     }
 }
